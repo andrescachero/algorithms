ce2c3423a5e479378fb74b396029b71c6616b34d
Andres Cachero <andres.cachero@gmail.com>
2015-01-19 15:00:33 -0300

    Fix example

--- ruby/tape_equilibrium.rb
+++ ruby/tape_equilibrium.rb
@@ -1,10 +1,3 @@
-def diff(ls,rs)
-  s = ls - rs
-  s = (0-1) * s if s < 0
-  s.to_i
-end
-
-
 def solution(a)
     # write your code in Ruby 1.9.3
     ls = 0
@@ -13,8 +6,7 @@ def solution(a)
     (0..a.length-2).each do |p|
       ls += a[p]
       rs -= a[p]
-      diff_p = diff(ls,rs)
-      p "#{diff_p}"
+      diff_p = (ls - rs).abs
       min_res = diff_p if min_res.nil? || diff_p < min_res
     end
     min_res
